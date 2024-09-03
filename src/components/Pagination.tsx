665ea5719bcb5f723f4f306a0e226511f8c36fa9
Bharatdwaj3 <badrikedar.jaydutt.456@gmail.com>
2024-09-03 18:50:06 +0530

    "bruder"

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactPaginate from 'react-paginate';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,7 +9,7 @@ type PaginationProps = {
     setPage: React.Dispatch<React.SetStateAction<number>>;
 };
 
-const Pagination = ({ totalPages, page, setPage }: PaginationProps) => {
+const Pagination = ({ totalPages, setPage }: PaginationProps) => {
     return (
         <>
             <ReactPaginate
@@ -28,34 +27,3 @@ const Pagination = ({ totalPages, page, setPage }: PaginationProps) => {
 };
 
 export default Pagination;
-=======
-import React from 'react';
-import ReactPaginate from 'react-paginate';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap";
-
-type PaginationProps = {
-    totalPages: number;
-    page: number;
-    setPage: React.Dispatch<React.SetStateAction<number>>;
-};
-
-const Pagination = ({ totalPages, page, setPage }: PaginationProps) => {
-    return (
-        <>
-            <ReactPaginate
-                className="pagination justify-content-center gap-4 mb-4"
-                previousLabel="Prev"
-                previousLinkClassName="btn btn-primary"
-                nextLabel="Next"
-                nextLinkClassName="btn btn-primary"
-                pageCount={totalPages ? totalPages : totalPages}
-                pageClassName="page-item"
-                pageLinkClassName="page-link"
-                onPageChange={(page) => { setPage(page.selected + 1); }}  />
-        </>
-    );
-};
-
-export default Pagination;
->>>>>>> dba47c2ce42c263ee9846ea648aed5d3474b3aae
